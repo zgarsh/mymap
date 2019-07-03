dc743462f3eac8aada3e5c57aa9f4298e763bd51
Zachary Garcia <garcia.zacharyw@gmail.com>
2019-07-02 22:06:16 -0700

    fixed styling by using tyleStyle variable instead of styled component for tile.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import Menu from './menu';
 import Tile from './tile';
 import Map from './map';
 import mapData from './data.json';
@@ -19,16 +17,6 @@ class Layout extends React.Component {
         {mapData.features.map( (item, index) => (
           <Tile title={item.properties.name} emoji={item.properties.emoji} text={item.properties.description} key={item.id}/>
         ))}
-        {/*<Tile title="Last Rites 💀🍹🔥" text={text} />
-        <Tile title="Hi Tops 🏳️‍🌈🏅🍺" text={text} />
-        <Tile title="Butter 🥤🚜🥫" text={text} />
-        <Tile title="Wilkommen 🇩🇪🍻🌿" text={text} />
-        <Tile title="The Willows 🥃🍔🍺" text={text} />
-        <Tile title="Flick's" text={text} />
-        <Tile title="Amigo room" text={text} />
-        <Tile title="" text={text} />
-        <Tile title="" text={text} />
-        */}
       </div>
     )
   };
--- src/components/tile.js
+++ src/components/tile.js
@@ -4,28 +4,25 @@ import {Button, Card} from 'react-bootstrap';
 
 // import Style from './layout'
 
-const StyledTile = styled.div`
-  position: sticky;
-  margin: 10px;
-  border-color: blue;
-  border-radius: 30px !important;
-  // background-color: rgb(230, 230, 230, 0.5);
-  // z-index: 2;
-  width: 20rem;
-`
+const tileStyle = {
+  position: 'sticky',
+  margin: '10px',
+  borderRadius: '3px',
+  backgroundColor: 'rgb(230, 230, 230, 0.5)',
+  zIndex: 2,
+  width: '20rem',
+}
 
 
 class Tile extends React.Component{
   render(){
     return(
-      <StyledTile>
-        <Card>
+        <Card style={tileStyle}>
           <Card.Body>
             <Card.Title><a href="#">{this.props.title}  {this.props.emoji}</a></Card.Title>
             <Card.Text>{this.props.text}</Card.Text>
           </Card.Body>
         </Card>
-      </StyledTile>
   );
   }
 }
