f1740704d97498ebd4812cdf534edcab4d511959
Zachary Garcia <garcia.zacharyw@gmail.com>
2019-08-04 22:25:34 -0700

    trying to change category styling based on state

--- src/components/category.js
+++ src/components/category.js
@@ -3,29 +3,37 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const CategoryStyle = styled.div`
-    position: relative;
-    float: right;
-    margin: 5px;
-    padding: 5px 10px;
-    border-radius: 10px;
-    background: rgb(230, 230, 230, 0.7);
-    z-index: 2;
-    height: 35px;
-    text-align: center;
-    font-size: 20;
+// let colorStatus = {{this.props.isActive}}
 
-    :hover {
-        background: rgb(105, 182, 223, 0.6);
-        cursor: pointer;
-    }
+const CategoryStyle = styled.div`
+  position: relative;
+  float: right;
+  margin: 5px;
+  padding: 5px 10px;
+  border-radius: 10px;
+  /* background: rgb(230, 230, 230, 0.7); */
+  z-index: 2;
+  height: 35px;
+  text-align: center;
+  font-size: 20;
+
+  /* :hover {
+      background: rgb(105, 182, 223, 0.6);
+      cursor: pointer;
+  } */
+
+  background-color: ${(props) =>  props.isActive ? "rgb(230, 230, 230, 0.7)" : "green"};
+
 `;
 
 
 class CategoryCard extends React.Component{
   constructor(props){
     super(props);
-    // this.state = { isActive: false};
+
+    // this.state = {
+    //   activeCategories: this.props.activeCategories,
+    // };
 
     // This bind is necessary to make `this` work in the callback in handleClick()
     // this.handleClick = this.handleClick.bind(this);
@@ -41,6 +49,9 @@ class CategoryCard extends React.Component{
   // }
 
   render(){
+
+
+    
     return(
         <CategoryStyle
           onClick={()=>this.props.onCategoryClick(this.props.category)}
--- src/components/listcategories.js
+++ src/components/listcategories.js
@@ -34,7 +34,7 @@ class CategoryList extends React.Component{
           // onClick={()=>{console.log('click??')}}
           // onClick={(e) => this.handleClick(e)}
           onCategoryClick={this.props.onCategoryClick}
-          isActive={this.props.activeCategories[item]} // NEW!!!
+          isActive={this.props.activeCategories[item]}
         />
       ))}
         {/* <CategoryCard category={JSON.stringify(this.props.activeCategories)} /> */}
